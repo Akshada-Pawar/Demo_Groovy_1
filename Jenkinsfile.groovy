1f231cf6d44d10cbaa9b4168b2d7647aafff904c
Akshada-Pawar <akshadapawar26297@gmail.com>
2021-02-07 22:58:10 +0530

    Added jenkins file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+node{
+        
+        stage('Get Latest Code'){
+            deleteDir()
+            checkout scm
+        }
+        stage('build'){
+                echo "Building..."
+                docker.image('python:3.5.1').inside{
+                    sh 'python --version'
+                    //sh 'pip install -r requirements.txt'
+                    sh 'python -m py_compile src/library.py'
+                stash(name: 'compiled-results', includes: 'src/*.py*')
+                }
+                
+                echo "Build Successful"
+        }
+        stage('test'){
+            echo "Testing..."
+            def testError = null
+            try{
+                docker.image('python:3.5.1').inside{
+                sh ' python src/library_test.py '
+            }
+            }
+            catch(err){
+                testError = err
+                currentBuild.result = 'FAILURE'
+            }
+            echo "Test Successful"
+        }
+}
